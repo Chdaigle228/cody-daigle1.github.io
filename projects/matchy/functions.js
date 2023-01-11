d8bd70fe58c45a118946ed92457a54cc671b0802
Chdaigle228 <cody.h.daigle@gmail.com>
2023-01-11 22:40:28 +0000

    Matchy Pt2: Remove

--- projects/matchy/functions.js
+++ projects/matchy/functions.js
@@ -58,13 +58,52 @@ function search(array, string){
 //////////////////////////////////////////////////////////////////////
 // Step 2 - Replace //////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
+/**
+ * ## Step 2 - Replace
+ 1. Write a function declaration called `replace` with a signature of `replace(animals, name, replacement) { //... } ` that:
+   - Takes 3 parameters, an Array of animals, a String representing  the name of an animal on which to perform a search, and an Object that represents the replacement animal.
+   - If an animal with that name exists within the `animals` Array, replace it's entire Object with the replacement Object.
+   - Otherwise do nothing.
+ 2. Preview the `index.html` page to test it on the website
+ */
+//str.replace(/[' ']/g, '-')
 
-
+//animals is an array of animals
+//name is used to perform the search
+//replacement is an object that represents the replacement animal
+var replace = function(animals, name, replacement){
+    for(let i = 0; i < animals.length; i++){
+        if(name === animals[i].name){
+            animals[i] = replacement
+        }
+    }
+}
 
 //////////////////////////////////////////////////////////////////////
 // Step 3 - Remove ///////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
-
+/**
+ * ## Step 3 - Remove
+ 1. Write a function declaration called `remove` with a signature of `remove(animals, name)` that:
+   - Takes 2 parameters, an Array of animals, and a name of an animal on which to perform a search.
+   - If an animal with that name exists within the `animals` Array, remove it.
+ 2. Test that it works on the website.
+ */
+/**
+ * animals is an array of animals
+ * name of an animal on which to perform a search
+ */
+ var remove = function(animals, name){
+    //iterate through animals array
+    for(let i = 0; i < animals.length; i++){
+        //check if name exists in the array of objects
+        if(name === animals[i].name){
+            //if it does then delete it
+            delete animals[i]
+            //else it does not
+        }
+    }
+ }
 
 
 //////////////////////////////////////////////////////////////////////
